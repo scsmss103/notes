b2190dd7281ab765d18db6e8affcdb6a538dff16
scsm <s.schlumpf@gmail.com>
2023-01-18 01:18:46 +0800

    changes

--- index.js
+++ index.js
@@ -15,3 +15,43 @@ return
 function navBtn(){
     alert('this is nav btn')
 }
+
+/*window.noteCardClick = function noteCardClick(e){
+    //alert('thats click')
+    //console.log('click fired')
+    //console.log(e.currentTarget.querySelector('.noteText'))
+    const txt = e.currentTarget.querySelector('.noteText')
+    const pin = e.currentTarget.querySelector('.pinContainer')
+    const action = e.currentTarget.querySelector('.actionContainer')
+
+    pin.style.visibility = 'visible'
+    action.style.visibility = 'visible'
+    txt.focus()
+}
+
+window.noteCardBlur = function noteCardBlur(e){
+    console.log('blur fired')
+    const txt = e.currentTarget.querySelector('.noteText')
+    const pin = e.currentTarget.querySelector('.pinContainer')
+    const action = e.currentTarget.querySelector('.actionContainer')
+    pin.style.visibility = 'hidden'
+    action.style.visibility = 'hidden'
+}*/
+
+window.noteCardFocus = function noteCardFocus(e){
+    const parent = e.currentTarget.parentElement
+    const pin = parent.querySelector('.pinContainer')
+    const action = parent.querySelector('.actionContainer')
+
+    pin.style.visibility = 'visible'
+    action.style.visibility = 'visible'
+}
+
+window.noteCardFocusOut = function noteCardFocusOut(e){
+    const parent = e.currentTarget.parentElement
+    const pin = parent.querySelector('.pinContainer')
+    const action = parent.querySelector('.actionContainer')
+
+    pin.style.visibility = null
+    action.style.visibility = null
+}
