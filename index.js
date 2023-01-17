4fe9fb3f0a461adcd416cf11ca3e2cb58614d407
scsm <s.schlumpf@gmail.com>
2023-01-18 01:59:35 +0800

    changes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import {setHeight} from './utils.js'
 
 window.initPage = function initPage(){
-    console.log('init page')
     setHeight('main')
-    return
 }
 
 
@@ -15,28 +13,6 @@ return
 function navBtn(){
     alert('this is nav btn')
 }
-
-/*window.noteCardClick = function noteCardClick(e){
-    //alert('thats click')
-    //console.log('click fired')
-    //console.log(e.currentTarget.querySelector('.noteText'))
-    const txt = e.currentTarget.querySelector('.noteText')
-    const pin = e.currentTarget.querySelector('.pinContainer')
-    const action = e.currentTarget.querySelector('.actionContainer')
-
-    pin.style.visibility = 'visible'
-    action.style.visibility = 'visible'
-    txt.focus()
-}
-
-window.noteCardBlur = function noteCardBlur(e){
-    console.log('blur fired')
-    const txt = e.currentTarget.querySelector('.noteText')
-    const pin = e.currentTarget.querySelector('.pinContainer')
-    const action = e.currentTarget.querySelector('.actionContainer')
-    pin.style.visibility = 'hidden'
-    action.style.visibility = 'hidden'
-}*/
 
 window.noteCardFocus = function noteCardFocus(e){
     const parent = e.currentTarget.parentElement
--- utils.js
+++ utils.js
@@ -1,5 +1,4 @@
 export function setHeight(cls,margin=10){
-    console.log('setHeight fired')
     const elems = document.querySelectorAll(`.${cls}`)
     elems.forEach((x)=>{
         var height = window.innerHeight - x.offsetTop - margin
