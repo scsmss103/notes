827629870240a5ff169dd472a9ba512410022d09
scsm <s.schlumpf@gmail.com>
2023-01-20 14:39:01 +0800

    changes

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import { setHeight } from './utils.js'
 
+
+const localData = JSON.parse(localStorage.getItem('notes'))
+
 window.initPage = function initPage() {
     setHeight('main')
+    console.log(localData)
 }
 
 
 
-function createPinnedNotes() {
-    return
+function createNoteCards(data) {
+    const tmplCard = document.querySelector('#tmpl_card')
+    const cards = document.createDocumentFragment()
+    const cardsCont = document.createElement('div')
+    cardsCont.classList.add('noteCards')
+
+    data.forEach((x)=>{
+        var tmpcard = tmplCard.cloneNode(true)
+    })
+    
+
 }
+
 
 function navBtn() {
     alert('this is nav btn')
@@ -36,7 +50,7 @@ function toggleColorPicker(e) {
     //close all other picker first
     const nearestNoteCar = e.currentTarget.closest('.noteCard')
     const allColorPicker = nearestNoteCar.querySelectorAll('.colorPicker')
-    allColorPicker.forEach((x)=>{
+    allColorPicker.forEach((x) => {
         x.style.visibility = null
     })
     //open color picker
@@ -58,6 +72,42 @@ function mouseLeaveColorPicker(e) {
     e.currentTarget.style.visibility = null
 }
 
+function maximizeCard(e) {
+    const card = e.currentTarget.closest('.noteCard')
+    const picker = card.querySelectorAll('.colorPicker')
+    if (Array(...card.classList).includes('maximized')) {
+        card.classList.remove('maximized')
+        picker.forEach((x) => {
+            x.classList.remove('maximized')
+        })
+    } else {
+        card.classList.add('maximized')
+        picker.forEach((x) => {
+            x.classList.add('maximized')
+        })
+    }
+}
+
+function bgColorClick(e) {
+    const card = e.currentTarget.closest('.noteCard')
+    const classes = Array(...e.currentTarget.classList)
+    const colors = ['red', 'blue', 'yellow', 'black', 'white']
+    const color = colors.find((x) => {
+        if (classes.includes(x)) {
+            return true
+        }
+        return false
+    })
+    const cardColor = colors.find((x) => {
+        if (Array(...card.classList).includes(x)) {
+            return true
+        }
+        return false
+    })
+    card.classList.remove(cardColor)
+    card.classList.add(color)
+}
+
 //eventListener
 const pickersIcon = document.querySelectorAll('.clrPicker')
 pickersIcon.forEach((x) => {
@@ -67,4 +117,14 @@ pickersIcon.forEach((x) => {
 const colorPicker = document.querySelectorAll('.colorPicker')
 colorPicker.forEach((x) => {
     x.addEventListener('mouseover', (e) => { mouseOverColorPicker(e) })
+})
+
+const maximize = document.querySelectorAll('.maximize')
+maximize.forEach((x) => {
+    x.addEventListener('click', (e) => { maximizeCard(e) })
+})
+
+const color = document.querySelectorAll('.bg-color')
+color.forEach((x) => {
+    x.addEventListener('click', (e) => { bgColorClick(e) })
 })
