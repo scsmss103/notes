06bc5650ee8d55c2994a72051ce07fe39d388172
scsm <s.schlumpf@gmail.com>
2023-03-21 00:57:31 +0800

    changes

--- utils.js
+++ utils.js
@@ -1,7 +1,15 @@
-export function setHeight(cls,margin=10){
+export function setHeight(cls, margin = 10) {
     const elems = document.querySelectorAll(`.${cls}`)
-    elems.forEach((x)=>{
+    elems.forEach((x) => {
         var height = window.innerHeight - x.offsetTop - margin
         x.style.height = `${height}px`
     })
+}
+
+export function randowmHex(no = 8) {
+    var str = ''
+    for (let i = 0; i <= no; i++) {
+        str += Math.floor(Math.random() * 36).toString(36)
+    }
+    return str
 }
